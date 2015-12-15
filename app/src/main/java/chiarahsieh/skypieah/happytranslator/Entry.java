7aac3c48f2efc198018629e49eb898279fa93d47
chiarahsieh <chiarahsieh@via.com.tw>
2015-12-15 11:29:32 +0800

    Initial commit

---
+++ app/src/main/java/chiarahsieh/skypieah/happytranslator/Entry.java
@@ -0,0 +1,14 @@
+package chiarahsieh.skypieah.happytranslator;
+
+/**
+ * Created by ChiaraHsieh on 2015/12/15.
+ */
+public class Entry {
+
+    public int index;
+    public String chi;
+    public String eng;
+    public String twn;
+
+
+}
